b46c729ed1b30f872e3a3c840b6ef7946860aff8
mnazrinasari <queckey2@gmail.com>
2025-01-15 01:28:10 +0800

    updates

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 2,
+  retries: process.env.CI ? 5 : 2,
   /* Opt out of parallel tests on CI. */
   workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
